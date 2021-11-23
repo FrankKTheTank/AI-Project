55edd5ae5a27f82f0910ad846545eab303f44a90
Frank <FKnes@gmx.de>
2021-11-23 11:02:11 +0100

    added loadPics_trainModel.py

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 import streamlit as st
-class_names =['daisy', 'dandelion','roses','sunflowers','tulips']
+class_names =['x', 'y']
 
 def load_model():
-  model=tf.keras.models.load_model('my_model2.hdf5')
+  model=tf.keras.models.load_model('../MyModel/testModel')
   return model
 with st.spinner('Model is being loaded..'):
   model=load_model()
@@ -14,13 +14,15 @@ st.write("""
          )
 
 file = st.file_uploader("Please upload an brain scan file", type=["jpg", "png"])
+
 import cv2
 from PIL import Image, ImageOps
 import numpy as np
 st.set_option('deprecation.showfileUploaderEncoding', False)
+
 def import_and_predict(image_data, model):
     
-        size = (180,180)    
+        size = (200,200)
         image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
         image = np.asarray(image)
         img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -37,14 +39,26 @@ if file is None:
 else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
+    #img = image.load_img(image, target_size=(200,200,3))
     predictions = import_and_predict(image, model)
     score = tf.nn.softmax(predictions[0])
     score = score *100
+    st.write(score)
+    st.write(predictions)
     #st.write(score)
-    #st.write(predictions)
-    #st.write(score)
-    st.write(
-    "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], np.max(score))
-)
+    #X = st.image.img_to_array(img)
+    #X = np.expand_dims(X,axis=0)
+    #images = np.vstack([X])
+
+    #val = model.predict(image)
+
+    if predictions == 0:
+         st.write(
+            "This image most likely belongs to Oben with a {:.2f} percent confidence."
+                .format(np.max(score)))
+    else:
+         st.write(
+    "This image most likely belongs to Unten with a {:.2f} percent confidence."
+    .format(np.max(score)))
+
 
---
+++ src/loadPics_trainModel.py
@@ -0,0 +1,84 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing import image
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import cv2
+import os
+
+
+#img = image.load_img("C:/Users/Frank/Desktop/Pics/Defekt/yellowBrick.PNG")
+#plt.imshow(img)
+#plt.show()
+
+#print(cv2.imread("C:/Users/Frank/Desktop/Pics/yellowBrick.PNG").shape)
+
+
+train = ImageDataGenerator(rescale=1/255)
+validation = ImageDataGenerator(rescale=1/255)
+
+trainDataset = train.flow_from_directory('C:/Users/Frank/Desktop/Pics/3003 Brick 2x2',
+                                         target_size=(200, 200), batch_size=3, class_mode='binary')
+
+validationDataset = validation.flow_from_directory('C:/Users/Frank/Desktop/Pics/3003 Brick 2x2',
+                                         target_size=(200, 200), batch_size=3, class_mode='binary')
+
+#print(trainDataset.class_indices)
+#print(trainDataset.classes)
+
+"""create a neronal network"""
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Conv2D(16,(3,3), activation='relu',input_shape=(200,200,3)), # 16 filter with each size 3*3
+  tf.keras.layers.MaxPool2D(2,2),
+  tf.keras.layers.Conv2D(32,(3,3), activation='relu',input_shape=(200,200,3)), # 32 filter
+  tf.keras.layers.MaxPool2D(2,2),
+  tf.keras.layers.Conv2D(64,(3,3), activation='relu',input_shape=(200,200,3)), # 64 filter
+  tf.keras.layers.MaxPool2D(2,2),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(units=512, activation="relu"),
+  tf.keras.layers.Dense(units=1, activation="sigmoid")
+])
+
+"""compile model"""
+model.compile(loss="binary_crossentropy", optimizer= tf.keras.optimizers.SGD() , metrics=["accuracy"])
+
+"""train model"""
+model.fit(trainDataset, batch_size=128, epochs=10, validation_data=validationDataset)
+
+model.save('C:/Users/Frank/Desktop/MyModel/testModel')
+
+dir_path= 'C:/Users/Frank/Desktop/Pics/Test'
+
+for i in os.listdir(dir_path):
+    img = image.load_img(dir_path + '//' + i, target_size=(200,200,3))
+    plt.imshow(img)
+    plt.show()
+
+    X = image.img_to_array(img)
+    X = np.expand_dims(X,axis=0)
+    images = np.vstack([X])
+    val = model.predict(images)
+    if val == 0:
+        print('Oben')
+    else:
+        print('Unten')
+
+
+
+
+
+
+
+
+
+
+# Create a dataset.
+#dataset = keras.preprocessing.image_dataset_from_directory('C:/Users/Frank/Desktop/Pics', batch_size=1, image_size=(479, 329))
+
+# For demonstration, iterate over the batches yielded by the dataset.
+#for data, labels in dataset:
+#    print(data.shape)  # (64, 200, 200, 3)
+#    print(data.dtype)  # float32
+#    print(labels.shape)  # (64,)
+#    print(labels.dtype)  # int32
