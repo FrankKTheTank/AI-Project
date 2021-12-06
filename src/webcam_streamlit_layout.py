79234bafdac5835e68b0072f2a5973950dfcf0b1
FrauN <katharina-neufeld@gmx.de>
2021-12-06 10:43:53 +0100

    ui layout file added HS Logo added Minion Logo added

---
+++ src/webcam_streamlit_layout.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import base64 # used in set_bg_hack to encode bg image
+from datetime import date # used in write_image for file name
+import time # used in write_image for file name
+import cv2 # used in write_image for writing images
+
+
+def header(text):
+    html_temp = f"""
+    <h1 style = "color:#009ee3; text_align:center; font-weight: bold;"> {text} </h1>
+    </div>
+    """
+
+    st.markdown(html_temp, unsafe_allow_html = True)
+
+
+#def Logo_HS():
+ #   main_logo = "Flower.png"
+  #  main_logo_ext="png"
+   # st.markdown(
+    #    f"""
+     #   <img>
+      #  .reportview-container {{
+       #      img: url(data:image/{main_logo_ext};base64,{base64.b64encode(open(main_logo, "rb").read()).decode()})
+        # }}
+       #  </img>
+        # """,
+       # unsafe_allow_html=True
+    #)
+
+
