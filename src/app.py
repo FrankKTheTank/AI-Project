02b2db8dbfd9edffd3765f42d114e96c14ee7696
Frank <FKnes@gmx.de>
2021-12-27 13:57:33 +0100

    made test

--- src/app.py
+++ src/app.py
@@ -22,7 +22,7 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 
 def import_and_predict(image_data, model):
     
-        size = (200,200)
+        size = (200,200) #test
         image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
         image = np.asarray(image)
         img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
